1f5afc9a5c6ec41c414a433cf4f9c75314464670
WebAzizbek <azizbekweb3365@gmail.com>
2022-12-24 16:13:39 +0500

    add js

--- js/about.js
+++ js/about.js
@@ -52,3 +52,17 @@ checkBox.addEventListener("click", () => {
     freeMode: true,
     loop: true
   });
+
+  let barsBtn = document.querySelector(".bars-btn");
+let run = document.querySelector(".run");
+let x = document.querySelector(".run__header-x");
+
+barsBtn.addEventListener("click", () => {
+    run.style.translate = "0%";
+    run.style.opacity = "1";
+  });
+
+  x.addEventListener("click", () => {
+    run.style.translate = "-100%";
+    run.style.opacity = "0";
+  });
